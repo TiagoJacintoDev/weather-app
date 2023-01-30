6e11d804de0e75f7116b9c0f688ad5ace82ad77c
tiagojacintodev <tiagojacintodev@gmail.com>
2023-01-30 17:33:22 +0000

    Implement Search Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+import { getWeatherData } from "./services/api";
+import { useEffect, useState } from "react";
+import { WeatherAPIResponse } from "./types/WeatherApiResponse";
+import { SearchBar } from "./components/SearchBar";
+
 export const App = () => {
-  return <h1>Hello World</h1>;
+  const [weatherData, setWeatherData] = useState<WeatherAPIResponse>();
+  const [city, setCity] = useState("London");
+  const [temperatureUnit, setTemperatureUnit] = useState<"C" | "F">("C");
+  const [timeSpan, setTimeSpan] = useState<"daily" | "hourly">("daily");
+  const [isCityValid, setIsCityValid] = useState(true);
+
+  const changeCity = (city: string) => setCity(city);
+
+  const changeWeatherData = async () => {
+    try {
+      const weatherData = await getWeatherData(city);
+      setWeatherData(weatherData);
+      setIsCityValid(true);
+    } catch {
+      setIsCityValid(false);
+      setTimeout(() => setIsCityValid(true), 4 * 1000);
+    }
+  };
+
+  useEffect(() => {
+    changeWeatherData();
+  }, [city]);
+
+  console.log(weatherData);
+
+  return (
+    <main className="bg-blue-600 w-full h-full">
+      <SearchBar changeCity={changeCity} isCityValid={isCityValid} />
+    </main>
+  );
 };
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { FaSearch as SearchIcon } from "react-icons/fa";
+import { AiOutlineExclamation as ExclamationIcon } from "react-icons/ai";
+import { AiOutlineClose as CleanInputIcon } from "react-icons/ai";
+
+interface Props {
+  changeCity: (city: string) => void;
+  isCityValid: boolean;
+}
+
+export const SearchBar = ({ changeCity, isCityValid }: Props) => {
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!input) return;
+
+    changeCity(input);
+    setInput("");
+  };
+
+  return (
+    <div className="absolute top-5 right-5">
+      <form className="text-xl" onSubmit={handleSubmit}>
+        <button>
+          <SearchIcon
+            className="absolute left-4 top-1/2 -translate-y-1/2 hover:text-gray-500 hover:cursor-pointer"
+            size={23}
+          />
+        </button>
+        <input
+          className="py-4 px-14 rounded-md outline-none"
+          placeholder="Search for a city"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        {input && (
+          <CleanInputIcon
+            size={25}
+            className="absolute right-4 top-1/2 -translate-y-1/2 hover:text-gray-500 hover:cursor-pointer"
+            onClick={(e) => {
+              setInput("");
+              const inputElement = e.currentTarget
+                .previousSibling as HTMLInputElement;
+              inputElement.focus();
+            }}
+          />
+        )}
+      </form>
+      {!isCityValid && (
+        <div className="bg-red-500 absolute top-full flex items-center gap-1.5 rounded-md w-full text-white px-4 py-4">
+          <ExclamationIcon size={30} />
+          <span className="text-lg">Please enter a valid city name...</span>
+        </div>
+      )}
+    </div>
+  );
+};
