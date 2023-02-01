9e5d9b6c2db00c5fea9f4e298d49ae36760ff5ed
tiagojacintodev <tiagojacintodev@gmail.com>
2023-02-01 13:43:12 +0000

    Redefine container sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,10 +3,14 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      screens: {
-        mobile: "640px",
-        tablet: "960px",
-        desktop: "1280px",
+      container: {
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1100px",
+        },
       },
     },
   },
