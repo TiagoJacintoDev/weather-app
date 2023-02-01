8d517328040d166ca0073011c8ce968b2849c0b6
tiagojacintodev <tiagojacintodev@gmail.com>
2023-02-01 11:47:13 +0000

    Remove unnecessary screen queries in tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: "640px",
+        tablet: "960px",
+        desktop: "1280px",
+      },
+    },
   },
   plugins: [],
-}
+};
